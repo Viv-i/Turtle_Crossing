d080f551db46e737126b83312ed03ed9ec5ae61b
Viv-i <feraloue@gmail.com>
2022-10-29 10:42:53 +0800

    Rewrote code to enable collision detection

--- car.py
+++ car.py
@@ -2,36 +2,39 @@ from turtle import Turtle
 import random
 
 COLORS = ["red", 'blue', 'green', 'violet', 'grey', 'white', 'pink', 'yellow']
-MOVEMENT_SPEED = 15
-CARS = []
+MOVEMENT_SPEED = 20
 
 
-class Car(Turtle):
+class Car:
     def __init__(self):
-        super().__init__('square')
+        self.garage = []
+
+    def create_car(self):
+        new_car = Turtle('square')
         if random.randrange(1, 4) == 1:
-            if len(CARS) == 0:
-                self.y_spawn_location = random.choice([-200, -150, -100, 0, 50, 100, 200, 250])
-                self.shapesize(stretch_len=2, stretch_wid=1)
-                self.color(random.choice(COLORS))
-                self.penup()
-                self.goto(500, self.y_spawn_location)
-                CARS.append(self)
-            if len(CARS) <= 50:
-                self.y_spawn_location = random.choice([-200, -150, -100, 0, 50, 100, 200, 250])
-                if CARS[len(CARS) - 1].ycor() != self.y_spawn_location:
-                    self.shapesize(stretch_len=2, stretch_wid=1)
-                    self.color(random.choice(COLORS))
-                    self.penup()
-                    self.goto(500, self.y_spawn_location)
-                    CARS.append(self)
+            if len(self.garage) == 0:
+                new_car.y_spawn_location = random.choice([-200, -150, -100, 0, 50, 100, 200, 250])
+                new_car.shapesize(stretch_len=2, stretch_wid=1)
+                new_car.color(random.choice(COLORS))
+                new_car.penup()
+                new_car.goto(500, new_car.y_spawn_location)
+                self.garage.append(new_car)
+            if len(self.garage) <= 50:
+                new_car.y_spawn_location = random.choice([-200, -150, -100, 0, 50, 100, 200, 250])
+                if self.garage[len(self.garage) - 1].ycor() != new_car.y_spawn_location:
+                    new_car.shapesize(stretch_len=2, stretch_wid=1)
+                    new_car.color(random.choice(COLORS))
+                    new_car.penup()
+                    new_car.goto(500, new_car.y_spawn_location)
+                    self.garage.append(new_car)
                 else:
                     pass
                     print("didn't create due to overlap")
-        for car in CARS:
+        for car in self.garage:
             if car.xcor() < -500:
-                CARS.remove(car)
+                self.garage.remove(car)
+        print(len(self.garage))
 
     def move(self):
-        for car in CARS:
+        for car in self.garage:
             car.backward(MOVEMENT_SPEED)
